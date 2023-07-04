825f8167eada5f07abc569bdb39875e9bd1c69f1
pacograndez <jgrandec@emeal.nttdata.com>
2023-07-04 01:08:59 -0500

    proyecto express api con ts

---
+++ index.ts
@@ -0,0 +1,22 @@
+// const express = require("express");
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  // res.send('Hello World!')
+
+  res.status(401).json({
+    ok: false,
+    msn: "No hay token en la petición",
+  });
+
+  //   res.json({
+  //     ok: true,
+  //     msg: "Todo salió bien!",
+  //   });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
